5d884dad584b0c17d315324a4b85aca0087bcb01
gut3 <1753664673@qq.com>
2019-05-04 23:07:25 +0800

    version 1.3

--- Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/MainActivity.java
+++ Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.gut3.coursecard;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 
@@ -12,6 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.login://监听菜单按钮
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -38,6 +58,24 @@ public class MainActivity extends AppCompatActivity {
      */
     public void skipEdit(View view) {
         Intent intent = new Intent(MainActivity.this, EditActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     *  点击跳转登录页面
+     * @param item
+     */
+    public void skipLogin(MenuItem item) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     *  点击跳转注册页面
+     * @param item
+     */
+    public void skipRegister(MenuItem item) {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 }
