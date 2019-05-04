7562c1007d079d6a04d933f90126ece0a01c9daa
gut3 <1753664673@qq.com>
2019-05-04 18:24:29 +0800

    version 1.0

---
+++ Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gut3.coursecard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
