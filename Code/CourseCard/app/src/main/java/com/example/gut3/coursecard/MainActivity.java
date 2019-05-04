a5cb837001654e0715277b3ee958de5bb7aaee77
gut3 <1753664673@qq.com>
2019-05-04 22:41:09 +0800

    version 1.2

--- Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/MainActivity.java
+++ Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
      */
     public void skipAboutMe(View view) {
         Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     *  点击跳转到编辑页面
+     * @param view
+     */
+    public void skipEdit(View view) {
+        Intent intent = new Intent(MainActivity.this, EditActivity.class);
         startActivity(intent);
     }
 }
