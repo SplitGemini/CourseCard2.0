1c91920eb68f4b60287b3cfbfb624777da3ae90e
gut3 <1753664673@qq.com>
2019-05-04 19:39:15 +0800

    version 1.1

--- Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/CourseActivity.java
+++ Code/CourseCard/app/src/main/java/com/example/gut3/coursecard/CourseActivity.java
@@ -5,29 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CourseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_course);
     }
 
     /**
-     *  单击跳转到课程页面
+     *  单击跳转论坛页面
      * @param view
      */
-    public void skipCourse(View view) {
-        Intent intent = new Intent(MainActivity.this, CourseActivity.class);
+    public void skipForum(View view) {
+        Intent intent = new Intent(CourseActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
     /**
-     *  单击跳转到个人信息页面
+     *  单击跳转个人信息页面
      * @param view
      */
     public void skipAboutMe(View view) {
-        Intent intent = new Intent(MainActivity.this, AboutMeActivity.class);
+        Intent intent = new Intent(CourseActivity.this, AboutMeActivity.class);
         startActivity(intent);
     }
 }
